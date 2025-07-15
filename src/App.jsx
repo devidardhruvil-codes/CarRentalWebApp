ec5e62f2a1abdf8729b5b25b1b7379d3f0f1051b
DevidarDhruvil <Dhruvil.Devidar@technovate.in>
2025-07-15 16:27:55 +0530

    Home Page and Car List Page, My Booking Page Completed

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Cars from "./pages/Cars";
+import CarDetails from "./pages/CarDetails";
+import MyBookings from "./pages/MyBookings";
+import { useState } from "react";
+import Footer from "./components/Footer";
+
+const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith("/owner");
+
+  return (
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+      </Routes>
+
+      {!isOwnerPath && <Footer />}
+    </>
+  );
+};
+
+export default App;
