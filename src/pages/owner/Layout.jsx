519f5719601cc0618fd0156779ac75bbfd70e1bb
DevidarDhruvil <Dhruvil.Devidar@technovate.in>
2025-07-29 12:06:52 +0530

    Admin Dashboard Sidebar and basic layout implemented

---
+++ src/pages/owner/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Sidebar from "../../components/owner/Sidebar";
+import { Outlet } from "react-router-dom";
+import NavbarOwner from "../../components/owner/NavbarOwner";
+
+const Layout = () => {
+  return (
+    <div className="flex flex-col">
+      <NavbarOwner />
+      <div className="flex">
+        <Sidebar />
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
